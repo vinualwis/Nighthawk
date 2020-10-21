ac70c84b8979e8be4927b137fdb72fbb1796bcd6
Vinu Alwis <ramith.alwis@team.telstra>
2020-10-21 21:18:15 +1100

    Redirection to board in sign in page removed

--- src/components/SigninForm/index.js
+++ src/components/SigninForm/index.js
@@ -4,7 +4,6 @@ import LoginAction from '../Login/LoginAction';
 import { auth } from '../../services/firebase';
 import './index.css';
 import { withRouter } from 'react-router-dom';
-import { BOARD } from '../../constants/routes';
 import LoginError from '../Login/LoginError';
 
 class SignInForm extends React.Component {
@@ -34,10 +33,7 @@ class SignInForm extends React.Component {
   onSubmit = (e) => {
     const { email, password } = this.state;
     e.preventDefault();
-    auth.signInWithEmailAndPassword(email, password).then(() => {
-      // eslint-disable-next-line react/prop-types
-      this.props.history.push(BOARD);
-    }).catch((error) => {
+    auth.signInWithEmailAndPassword(email, password).catch((error) => {
       this.setState({error})
     });
   }
